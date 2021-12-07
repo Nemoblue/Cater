bf5b04afc8f4245845dfaa7307505792dbbfa419
scwuaa <scwuaa@connect.ust.hk>
2021-12-07 16:25:57 +0800

    basic UI and function of MeFragment

--- app/src/main/java/com/example/cater/ui/me/MeFragment.java
+++ app/src/main/java/com/example/cater/ui/me/MeFragment.java
@@ -1,10 +1,17 @@
 package com.example.cater.ui.me;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,12 +19,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.cater.R;
 import com.example.cater.databinding.FragmentMeBinding;
+import com.example.cater.ui.map.MapFragment;
+
+import java.io.InputStream;
+import java.net.URI;
 
 public class MeFragment extends Fragment {
 
     private MeViewModel meViewModel;
     private FragmentMeBinding binding;
+    private ImageView userIcon;
+    public static final int PICK_IMAGE = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,11 +41,28 @@ public class MeFragment extends Fragment {
         binding = FragmentMeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textMe;
+        //final TextView textView = binding.textMe;
         meViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
+            }
+        });
+        userIcon = root.findViewById(R.id.user_icon);
+        userIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+        final TextView userName = root.findViewById(R.id.user_name);
+        userName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userName.setText("TEST");
             }
         });
         return root;
@@ -41,5 +72,12 @@ public class MeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK){
+            android.net.Uri uri = data.getData();
+            userIcon.setImageURI(uri);
+        }
     }
 }
